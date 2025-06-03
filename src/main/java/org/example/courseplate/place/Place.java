2e0d0a6c223fc333fedfd390fb7fc48bdde1130f
KimMundo1 <801sml@gmail.com>
2025-06-03 10:52:49 +0900

    place, restaurants 병합

--- src/main/java/org/example/courseplate/place/Place.java
+++ src/main/java/org/example/courseplate/place/Place.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document("Place")
+@Document("restaurants")
 @Getter
 @Setter
 public class Place {
